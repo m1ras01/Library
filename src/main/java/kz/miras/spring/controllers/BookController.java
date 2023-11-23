4fb66274c10cc671d24c6d7c1708dfa5593e4cc5
m1ras01 <221930@astanait.edu.kz>
2023-11-24 04:12:00 +0600

    update BookController

--- src/main/java/kz/miras/spring/controllers/BookController.java
+++ src/main/java/kz/miras/spring/controllers/BookController.java
@@ -1,4 +1,25 @@
 package kz.miras.spring.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import kz.miras.spring.dao.bookDAO;
+
+@Controller
+@RequestMapping("/books")
 public class BookController {
+    private final bookDAO bookDAO;
+
+    @Autowired
+    public BookController(kz.miras.spring.dao.bookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) throws Exception{
+        model.addAttribute("kytap", bookDAO.index());
+        return "book/index";
+    }
 }
