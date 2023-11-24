e2759ca116f4207b0532892ec8090d2ea12531b0
m1ras01 <221930@astanait.edu.kz>
2023-11-24 16:28:30 +0600

    html page for adding book

--- src/main/java/kz/miras/spring/controllers/BookController.java
+++ src/main/java/kz/miras/spring/controllers/BookController.java
@@ -1,11 +1,10 @@
 package kz.miras.spring.controllers;
 
+import kz.miras.spring.models.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import kz.miras.spring.dao.bookDAO;
 import kz.miras.spring.dao.authorDAO;
 
@@ -33,5 +32,15 @@ public class BookController {
         return "book/show";
     }
 
+    @GetMapping("/new")
+    public String newBook(Model model){
+        model.addAttribute("books",new Book());
+        return "books/new";
+    }
 
+    @PostMapping()
+    public String create(@ModelAttribute("books") Book book){
+        bookDAO.save(book);
+        return "redirect:/books";
+    }
 }
